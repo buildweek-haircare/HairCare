274a7e071dded642f8a9dabf5732f80154f8a502
Jacqueline <jacquelineomollo@gmail.com>
2019-06-24 10:38:47 -0400

    hamburger menu js done.

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,1 +1,22 @@
 // dropdown functionality
+class Navdown {
+  constructor(element) {
+    this.element = element;
+
+    this.button = this.element.querySelector("div");
+
+    this.content = this.element.querySelector(".dropdown-content");
+
+    this.button.addEventListener("click", () => {
+      this.toggleContent();
+    });
+  }
+
+  toggleContent() {
+    this.content.classList.toggle("dropDown-hidden");
+  }
+}
+
+let dropdowns = document
+  .querySelectorAll(".dropdown")
+  .forEach(dropdown => new Navdown(dropdown));
